a27615b8fc748e167264d33d892668f4ed241cf8
Sential <sentialx@gmail.com>
2018-11-01 17:04:35 +0100

    Initial commit

---
+++ fuse.js
@@ -0,0 +1,59 @@
+const {
+  FuseBox,
+  WebIndexPlugin,
+  QuantumPlugin,
+  EnvPlugin,
+  CopyPlugin,
+  CSSPlugin,
+  SassPlugin,
+  CSSResourcePlugin
+} = require("fuse-box");
+
+const production = process.env.NODE_ENV === "dev" ? false : true;
+
+const config = {
+  homeDir: "src/",
+  cache: !production,
+  target: "browser@es6",
+  output: `build/$name.js`,
+  tsConfig: "./tsconfig.json",
+  sourceMaps: !production,
+  useTypescriptCompiler: true,
+  plugins: [
+    EnvPlugin({ NODE_ENV: production ? "production" : "development" }),
+    production &&
+      QuantumPlugin({
+        bakeApiIntoBundle: "app",
+        treeshake: true,
+        removeExportsInterop: false,
+        uglify: {
+          es6: true
+        }
+      }),
+    [SassPlugin(), CSSPlugin()],
+    WebIndexPlugin({
+      template: `src/resources/page/app.html`,
+      path: production ? "." : "/",
+      target: `app.html`
+    }),
+    CopyPlugin({
+      files: ["*.woff2", "*.png", "*.svg"],
+      dest: "assets",
+      resolve: production ? "./assets" : "/assets"
+    })
+  ]
+};
+
+const fuse = FuseBox.init(config);
+
+if (!production) {
+  fuse.dev({ httpServer: true });
+}
+
+const app = fuse.bundle("app").instructions("> [index.ts]");
+
+if (!production) {
+  app.hmr().watch();
+}
+
+fuse.run();
